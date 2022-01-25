ff10a5647cbf79da8b33dacb16d9a462f8c14dc3
Julian Zuniga <anzuti@gmail.com>
2022-01-25 15:54:59 -0500

    JAN25

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,73 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+// IDENTIFICACIÓN PARA LOS CONTROLADORES DE MOTOR EN EL DRIVERTRAIN
+
+public static final int MOTOR_RIGHT_0_ID = 2;
+public static final int MOTOR_RIGHT_1_ID = 3;
+public static final int MOTOR_LEFT_2_ID = 0;
+public static final int MOTOR_LEFT_3_ID = 1;
+
+
+// hola
+// IDENTIFICACION PARA EL JOYSTICK
+
+public static final int GAME_PAD = 0;
+public static final int ButtonSquare = 1;
+public static final int ButtonX = 2;
+public static final int ButtonCircle = 3;
+public static final int ButtonTriangle = 4;
+public static final int ButtonL1 = 5;
+public static final int ButtonR1 = 6;
+public static final int ButtonL2 = 7;
+public static final int ButtonR2 = 8;
+public static final int ButtonShare = 9;
+public static final int ButtonOptions = 10;
+public static final int ButtonL3 = 11;
+public static final int ButtonR3 = 12;
+public static final int ButtonPs = 13;
+public static final int ButtonTouchPad = 14;
+
+
+
+
+//CONSTANTES PARA AJUSTES DE SET POINT
+
+
+
+
+// CONSTANTES DE VELOCIDAD MAXIMA DE MOTORES DEL DRIVETRAIN
+
+public static final double dampenSpeed = 0.8;
+public static final double dampenturn = 0.6;
+public static final double DT_ROTATION_SPEED = 0.35;
+
+// CONSTANTES PARA MOTION PROFILING DEL DRIVETRAIN
+public static final double kMaxSpeedMetersPerSecond= 1.5;
+public static final double kMaxAccelerationMetersPerSecondSquared = 0.75;
+public static double dampenSpeedIntake = 1;
+
+// CONSTANTES PARA MOTORES DEL INTAKE
+
+public static int triggerID = 5;
+public static int intakeID = 4;
+
+
+
+
+
+
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,97 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+//// IMPORT SUBSYSTEMS ///////////////////////
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Intake;
+
+//// IMPORT COMMANDS //////////////////////////
+
+import frc.robot.commands.*;
+
+
+
+
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final Intake m_intake = new Intake();
+  private final DriveTrain m_drivetrain = new DriveTrain();
+  private final Joystick m_joystick = new Joystick(Constants.GAME_PAD);
+
+  private final Command m_autoCommand = new autoRoutine(m_drivetrain);
+ 
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  
+  //METODO CONSTRUCTOR PARA ROBOTCONTAINER
+  public RobotContainer() {
+
+    m_drivetrain.calibrateGyro();   
+    // Configure the button bindings
+    configureButtonBindings();
+
+    
+    //set Default Command
+   m_drivetrain.setDefaultCommand(new arcDrive(m_drivetrain, m_joystick));
+
+    
+  
+
+
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+
+    //final JoystickButton buttonA = new JoystickButton(m_joystick, Constants.ButtonA);
+    //buttonA.whileHeld(new arcDStraight(m_drivetrain, m_joystick));  
+    
+    //final JoystickButton buttonX = new JoystickButton(m_joystick, Constants.ButtonX);
+    //buttonX.whenPressed(new DriveDistancePID(m_drivetrain,5));  
+
+
+    //final JoystickButton buttonB = new JoystickButton(m_joystick, Constants.ButtonB);
+    //buttonB.whenPressed(new rotateTo(m_drivetrain,90));  
+
+    //final JoystickButton buttonY = new JoystickButton(m_joystick, Constants.ButtonY);
+    //buttonY.whenPressed(new DriveDistancePID(m_drivetrain,-5)); 
+    
+
+    final JoystickButton buttonSquare = new JoystickButton(m_joystick, Constants.ButtonSquare);
+    buttonSquare.whileHeld(new collectBall(m_intake));
+    
+    final JoystickButton buttonCircle = new JoystickButton(m_joystick, Constants.ButtonCircle);
+    buttonCircle.whileHeld(new collectBall(m_intake));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return m_autoCommand;
+    
+  }
+}
